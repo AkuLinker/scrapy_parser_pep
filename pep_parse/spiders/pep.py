429ab9401d5200842bbb0cadc284dc5090694856
AkuLinker <AkuLinker@gmail.com>
2022-05-08 23:44:51 +0300

    all peps info parse added

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,3 +1,5 @@
+import re
+
 import scrapy
 
 from pep_parse.items import PepParseItem
@@ -9,4 +11,16 @@ class PepSpider(scrapy.Spider):
     start_urls = ['http://peps.python.org/']
 
     def parse(self, response):
-        pass
+        peps = response.css('section#numerical-index tbody tr')
+        for pep_url in peps.css('td.num a::attr(href)'):
+            yield response.follow(pep_url, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        h1 = response.css('h1.page-title::text').get().strip()
+        number_of_pep, name_of_pep = re.split(' – ', h1)
+        data = {
+            'number': number_of_pep.replace('PEP ', ''),
+            'name': name_of_pep,
+            'status': response.css('dt:contains("Status") + dd::text').get(),
+        }
+        yield PepParseItem(data)
